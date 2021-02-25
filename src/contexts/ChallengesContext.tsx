e709d6c5622fcf07c3d94db158f1833dbad5fcce
Caio <caiodmvieiragithub@gmail.com>
2021-02-25 16:21:13 -0300

    feat: make leveling

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps  {
@@ -50,6 +51,25 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(null);
   }
 
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if(finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
+
   return (
     //Provider: Todos os elementos dentro do provider vão ter acesso aos dados daquele contexto
     <ChallengesContext.Provider 
@@ -61,7 +81,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
       levelUp,
       startNewChallenge,
       activeChallenge,
-      resetChallenge
+      resetChallenge,
+      completeChallenge
     }}>
       {children}
     </ChallengesContext.Provider>
